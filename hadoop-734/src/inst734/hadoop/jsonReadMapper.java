9405552ab681856aa4f56a66ad161b9b3484ec72
shrawanraina <shrawanraina@gmail.com>
2014-11-18 01:31:20 -0500

    updated for gz

--- hadoop-734/src/inst734/hadoop/jsonReadMapper.java
+++ hadoop-734/src/inst734/hadoop/jsonReadMapper.java
@@ -4,10 +4,9 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.StringReader;
 import java.util.StringTokenizer;
+import java.util.zip.GZIPInputStream;
 
-import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
-import org.apache.hadoop.mapred.OutputCollector;
 import org.apache.hadoop.mapreduce.Mapper;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
@@ -19,17 +18,17 @@ public class jsonReadMapper extends Mapper<Object, Text, Text, Text> {
 	//private final static LongWritable one = new LongWritable();
 	
 	public void map(Object key, Text value, Context output) throws IOException, InterruptedException {
-		if(value.equals(null) || value.getLength() == 0){
-			System.out.println("No File to Read");
-			return;
-		}
-		BufferedReader br = new BufferedReader(new StringReader(value.toString()));
-		String line = "";
-		String updId;
-		while ((line = br.readLine()) != null) {
-			JSONParser jsonParser = new JSONParser();
-			JSONObject jsonObject = null;
-			try {
+		try{
+			if(value.equals(null) || value.getLength() == 0){
+				System.out.println("No File to Read");
+				return;
+			}
+			BufferedReader br = new BufferedReader(new StringReader(value.toString()));
+			String line = "";
+			String updId;
+			while ((line = br.readLine()) != null) {
+				JSONParser jsonParser = new JSONParser();
+				JSONObject jsonObject = null;
 				jsonObject = (JSONObject) jsonParser.parse(line);
 				String text = (String) jsonObject.get("text");
 				Long id = (Long) jsonObject.get("id");
@@ -41,10 +40,11 @@ public class jsonReadMapper extends Mapper<Object, Text, Text, Text> {
 					String token = itr.nextToken();
 					output.write(new Text(token.toLowerCase()), new Text(updId));
 				}
-			} catch (ParseException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
 			}
 		}
-    }
+		catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 }
